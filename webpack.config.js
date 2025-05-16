f380467e04c9cb9dfb6a66d716a9519864fa7197
Oksana Bannova <oksanchik2170@yandex.ru>
2025-05-16 15:48:51 +0300

    commit3

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets:  [
+              ['@babel/preset-env']
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
